4a2a4207e18659549b629152af29be556ca0d7ff
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-13 20:42:41 -0600

    Initialize Xcode Project
    Commit skeleton Xcode/Swift related files
    
    Related to: none

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Darren Rambaud on 02/13/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
